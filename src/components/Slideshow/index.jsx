f2f8206eea32dc39a66417f57e989f6d6252e10b
3MM4NU3L98 <emmanuelsegeard@gmail.com>
2024-04-09 18:42:25 +0200

    slider fini

--- src/components/Slideshow/index.jsx
+++ src/components/Slideshow/index.jsx
@@ -1,37 +1,42 @@
-
-// import ArrowBackward from '../../assets/arrow_backward.svg'
-// import ArrowForward from '../../assets/arrow_forward.svg'
+import { useState } from 'react'
+import ArrowBackward from '../../assets/arrow_backward.svg'
+import ArrowForward from '../../assets/arrow_forward.svg'
 
 import './slideshow.scss'
 
 function Slideshow ({pictures}) {
+    const [slidesIndex, setSlidesIndex ] = useState(0)
 
-    let  slidesIndex = 0
-
-    // const slide = (direction) => {
-    //     if (direction === 'backward') {
-    //         slidesIndex--
-    //         if (slidesIndex < 0) { slidesIndex = pictures.length - 1 }
-    //     } else if (direction === 'foward') {
-    //         slidesIndex++
-    //         if (slidesIndex > pictures.length - 1) { slidesIndex = 0 }
-    //     }
-    // }
+    const slide = (direction) => {
+        if (direction === 'backward') {
+            setSlidesIndex((prevSlide) => (prevSlide === 0 ? pictures.length - 1 : prevSlide - 1));
+        } else if (direction === 'forward') {
+            setSlidesIndex((prevSlide) => (prevSlide === pictures.length - 1 ? 0 : prevSlide + 1));
+        }
+    }
    
-    console.log(slidesIndex)
-    console.log(pictures[slidesIndex])
-
     return (
         <div className="slideshow">
-            <img className='slideshow_img' src={pictures[slidesIndex]} alt= {`Photographie ${slidesIndex+1} sur ${pictures.length}`} />
+            <img
+                className='slideshow_img'
+                src={pictures[slidesIndex]}
+                alt= {`Photographie ${slidesIndex + 1} sur ${pictures.length}`}
+             />
             <div className='slideshow_arrow'>
-                {/* <img className='slideshow_arrow_backward' src={ArrowBackward} alt="Flèche vers la précédante" onClick={slide('backward')}/> */}
-                {/* <img className='slideshow_arrow_forward' src={ArrowForward} alt="Flèche vers la suivante" onClick={slide('forward')}/> */}
+                <img onClick={() => slide('backward')}
+                    className='slideshow_arrow_backward'
+                    src={ArrowBackward}
+                    alt="Flèche vers la photographie précédante"
+                />
+                <img onClick={() => slide('forward')}
+                    className='slideshow_arrow_forward'
+                    src={ArrowForward}
+                    alt="Flèche vers la photographie suivante"
+                />
             </div>
-            <div className="slideshow_caption">{`${++slidesIndex}/${pictures.length}`}</div>
+            <div className="slideshow_caption">{`${slidesIndex + 1}/${pictures.length}`}</div>
         </div> 
     )
 }
 
 export default Slideshow
-
