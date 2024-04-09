b13bc0ff0cc2c8d2d8f27cc3bda8a65addcf12c9
3MM4NU3L98 <emmanuelsegeard@gmail.com>
2024-04-09 19:56:24 +0200

    dernier detail

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,20 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import Logo from '../../assets/logoColored.svg'
 import './header.scss'
 
 function Header() {
-  return (
-    <header>
-      <Link to="/">
-        <img src={Logo} alt='Logo de Kasa' />
-      </Link>
-      <nav>
-        <Link to="/">Accueil</Link>
-        <Link to="/about">A Propos</Link>
-      </nav>
-    </header>
-  )
+    let location = useLocation();
+    return (
+        <header>
+              <Link to="/">
+                  <img src={Logo} alt='Logo de Kasa' />
+              </Link>
+          <nav>
+              <Link to="/" className={location.pathname === "/" ? 'linkDecoration' : ''}>Accueil</Link>
+              <Link to="/about" className={location.pathname === "/about" ? 'linkDecoration' : ''}>A Propos</Link>
+          </nav>
+      </header>
+    )
 }
 
 export default Header
